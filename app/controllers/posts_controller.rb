4bca6491ee1a8126e7bc3eab9591de1527f54ab1
jkatem <jenmiao@gmail.com>
2020-07-05 22:09:08 -0700

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,5 +20,6 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    # category_ids:[] => post_params expects key of category_ids with value of array
   end
 end
